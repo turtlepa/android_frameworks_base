cce682d2d43db56463cba27b5dea43031e24f6f5
David Lamontagne <DJLaMontagneIII@gmail.com>
2014-09-29 20:36:08 -0400

    Serp
    Change-Id: If997fdb6994f2b384ee725455c456911b738853a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4222,6 +4222,13 @@ public final class Settings {
         public static final String RECENT_CARD_TEXT_COLOR = "recent_card_text_color";
 
         /**
+         * Custom Recent toggle
+         *
+         * @hide
+         */
+        public static final String CUSTOM_RECENT_TOGGLE = "custom_recent_toggle";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
